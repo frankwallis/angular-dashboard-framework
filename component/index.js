eeaef93750515df0750757197ce92ff5e33ff927
de <daniel.ecer@jhc.co.uk>
2014-08-15 16:48:05 +0100

    now using source version rather than minified version

---
+++ component/index.js
@@ -0,0 +1,15 @@
+require("../src/scripts/adf");
+require("../src/scripts/dashboard");
+require("../src/scripts/provider");
+require("../src/scripts/sortable");
+require("../src/scripts/widget-content");
+require("../src/scripts/widget");
+
+angular.module('adf')
+.run(function ($templateCache) {
+    $templateCache.put("../src/templates/dashboard-edit.html", require("../src/templates/dashboard-edit.html"))
+    $templateCache.put("../src/templates/dashboard.html", require("../src/templates/dashboard.html"))
+    $templateCache.put("../src/templates/widget-add.html", require("../src/templates/widget-add.html"))
+    $templateCache.put("../src/templates/widget-edit.html", require("../src/templates/widget-edit.html"))
+    $templateCache.put("../src/templates/widget.html", require("../src/templates/widget.html"))
+});
