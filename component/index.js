9dc00a0084da358da7428b5ae7765de6cffe89db
Frank Wallis <frank.wallis@jhc.co.uk>
2014-08-29 11:57:31 +0100

    feat(foundation): switch to foundation

--- component/index.js
+++ component/index.js
@@ -7,9 +7,9 @@ require("../src/scripts/widget");
 
 angular.module('adf')
 .run(function ($templateCache) {
-    $templateCache.put("../src/templates/dashboard-edit.html", require("../src/templates/dashboard-edit.html"))
-    $templateCache.put("../src/templates/dashboard.html", require("../src/templates/dashboard.html"))
-    $templateCache.put("../src/templates/widget-add.html", require("../src/templates/widget-add.html"))
-    $templateCache.put("../src/templates/widget-edit.html", require("../src/templates/widget-edit.html"))
-    $templateCache.put("../src/templates/widget.html", require("../src/templates/widget.html"))
+    $templateCache.put("../src/templates/dashboard-edit.html", require("../src/templates/foundation/dashboard-edit.html"))
+    $templateCache.put("../src/templates/dashboard.html", require("../src/templates/foundation/dashboard.html"))
+    $templateCache.put("../src/templates/widget-add.html", require("../src/templates/foundation/widget-add.html"))
+    $templateCache.put("../src/templates/widget-edit.html", require("../src/templates/foundation/widget-edit.html"))
+    $templateCache.put("../src/templates/widget.html", require("../src/templates/foundation/widget.html"))
 });
